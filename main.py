0eccce4b3ebfb8ec3c3142d686449e97ba63a1d7
bolddestroyer <banski.dorian@gmail.com>
2025-02-28 14:14:59 +0100

    minor adjustments

--- main.py
+++ main.py
@@ -25,9 +25,8 @@ def check_package_status():
             if "pip" in package:
                 print("A new version of pip is available.")
 
-    except (subprocess.CalledProcessError, FileNotFoundError) as e:
+    except (subprocess.CalledProcessError, FileNotFoundError, Exception) as e:
         messagebox.showerror("Package error", "Error: {e}")
-        # In case of errors, show message box and return nothing (stop the process)
         return
 
 
@@ -48,21 +47,16 @@ def get_python_version():
 
 def generate_exe_file(python_file_path):
     start_time = time.time()
+    python_file_regexp = re.compile(".*.py$")
 
     check_package_status()
 
-    # Regexp for a Python file
-    python_file_regexp = re.compile(".*.py$")
-
-    # If a Python file is selected
     if python_file_regexp.search(python_file_path):
         print(python_file_path)
         try:
-            # Check if the selected Python file has no syntax errors
             py_compile.compile(python_file_path, doraise=True)
         except py_compile.PyCompileError as e:
             messagebox.showerror("Error", f"Error with the selected file:\n{e}.")
-            # In case of errors, show message box and return nothing (stop the process)
             return
 
         output_file_name = os.path.splitext(os.path.basename(python_file_path))[0]
@@ -72,16 +66,14 @@ def generate_exe_file(python_file_path):
             output_file_name,
         )
 
-        # If the working directory exists, remove it with all its content
         if os.path.exists(working_directory):
             shutil.rmtree(working_directory)
 
         os.makedirs(working_directory)
-        # Change the working directory
         os.chdir(working_directory)
 
         if working_directory not in os.getcwd():
-            messagebox.showerror("Error", f"Issue with the working directory:\n{working_directory}")
+            messagebox.showerror("Error", f"Issue with the working directory: {working_directory}")
             return
 
         python_version = get_python_version()
@@ -114,30 +106,24 @@ def generate_exe_file(python_file_path):
 
         custom_messagebox(exe_output_directory)
 
-    # If no file is selected
     elif python_file_path == "No file selected":
         messagebox.showerror("Error", "Select a file.")
 
-    # If selected file is not a python file
     elif not python_file_regexp.search(python_file_path):
         messagebox.showerror("Error", "Only Python files can be processed (extension '.py').")
 
 
 def open_file_explorer(label):
-    # Open the file explorer, save file path as a variable
     file_path = filedialog.askopenfilename()
     if file_path:
-        # Set the file path as new text for the label
         label.config(text=file_path)
 
 
-# Open the file explorer with the directory of the generated EXE file
 def open_exe_file_directory(python_file_path):
     os.startfile(os.path.dirname(python_file_path))
     sys.exit()
 
 
-# Custom message box with buttons to close and open the file explorer
 def custom_messagebox(python_file_path):
     window = tkinter.Tk()
     window.title("Info")
@@ -155,15 +141,8 @@ def custom_messagebox(python_file_path):
     btn_open_explorer.grid(row=1, column=1, padx=10, pady=10)
 
 
-def close_existing_window(window_title):
-    hwnd = win32gui.FindWindow(None, window_title)
-    if hwnd:
-        ctypes.windll.user32.PostMessageW(hwnd, 0x0010, 0, 0)
-
-
 def app_window():
     window_title = "generateEXE"
-    close_existing_window(window_title)
 
     window = tkinter.Tk()
     window.title(window_title)
@@ -179,5 +158,4 @@ def app_window():
     )
     btn_generate.grid(row=1, column=0, padx=10, pady=10)
 
-    # Loop the window, refresh after each change (e.g. change of value in label)
     window.mainloop()
